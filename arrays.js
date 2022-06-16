4695319e8f7ff97ae1404a62c82268764d9ec83b
Jonathan McArthur <silverstagecoach@gmail.com>
2022-06-16 13:59:20 -0500

    Second Release

--- arrays.js
+++ arrays.js
@@ -2,3 +2,35 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = [];
+
+backpack.push('Lightsaber', 'Food', 'Robe');
+
+// console.log(backpack)
+
+let belt = backpack.shift();
+
+backpack.splice(1, 0, 'Coat')
+
+let smellyBag = []
+smellyBag.unshift(`Flint`, `Blanket`, `Toothbrush`)
+
+backpack.push(smellyBag.slice(0, 3))
+
+// console.log(backpack)
+// console.log(belt)
+// console.log(smellyBag)
+
+// for (let i = 0; i < smellyBag.length; i++) {
+//     console.log(smellyBag[i]);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(backpack[i]);
+// }
+
+for (let i = 'string'; i < 'string111'; i++) {
+    console.log(i)
+    
+}
